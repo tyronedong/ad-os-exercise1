5b119937e378d705c1d45a906a83b91d9cfaac55
tyronedong <dtz1994@outlook.com>
2017-11-09 00:59:13 +0800

    socket

--- sub_exercise_1.py
+++ sub_exercise_1.py
@@ -1,20 +1,59 @@
 import socket
 import sys
 
-host = 'www.google.com'
-port = 80
+# host = 'www.google.com'
+# port = 80
+#
+# try:
+#     remote_ip = socket.gethostbyname(host)
+#
+# except socket.gaierror:
+#     # could not resolve
+#     print('Hostname could not be resolved. Exiting')
+#     sys.exit()
+#
+# print('Ip address of ' + host + ' is ' + remote_ip)
 
-try:
-    remote_ip = socket.gethostbyname(host)
 
-except socket.gaierror:
-    # could not resolve
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
+def server():
+    HOST = ''  # Symbolic name meaning all available interfaces
+    PORT = 5678  # Arbitrary non-privileged port
 
-print('Ip address of ' + host + ' is ' + remote_ip)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+
+    print('Socket bind complete')
+
+    s.listen(10)
+    print('Socket now listening on port: ' + str(PORT))
+
+    # now keep talking with the client
+    while True:
+        # wait to accept a connection - blocking call
+        conn, addr = s.accept()
+        print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+        data = conn.recv(1024)
+        reply = 'OK...' + data
+        if not data:
+            break
+
+        conn.sendall(reply)
+
+    conn.close()
+    s.close()
+
 
 if __name__ == '__main__':
-    # if len(sys.argv) == 1:
-    # print(sys.argv[1])
+    if len(sys.argv) == 1:
+        print('method not specified(server or client)')
+        exit()
+    server()
+    print('')
     pass
