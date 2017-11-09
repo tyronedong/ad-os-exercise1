e6bce1a196201b9879df2958f5612ec1058230c4
tyronedong <dtz1994@outlook.com>
2017-11-09 15:07:36 +0800

    modify sub e2

--- sub_exercise_2.py
+++ sub_exercise_2.py
@@ -1,3 +1,56 @@
+import socket
+import sys
+
+def server():
+    HOST = ''  # Symbolic name meaning all available interfaces
+    PORT = 8888  # Arbitrary non-privileged port
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(10)
+    print('Socket now listening on port: ' + str(PORT))
+
+    # wait for client connect
+    conn, addr = s.accept()
+    print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+    # now keep talking with the client
+    while True:
+        # wait to accept a connection - blocking call
+        data = conn.recv(4096)
+
+        if not data:
+            print('Disconnected with ' + addr[0] + ':' + str(addr[1]))
+            break
+
+        cmd_recv = str(data, encoding='utf-8').strip()
+
+        if cmd_recv == 'go':
+            # reply = str(rd.randint(1, 6))
+            pass
+        else:
+            reply = 'command not defined: ' + cmd_recv
+
+        conn.sendall(bytes(reply, encoding='utf-8'))
+        print('replt to client: ' + reply)
+
+    conn.close()
+    s.close()
+
+def client():
+    host = 'localhost'
+    port = 8888
+
+    # create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish connect
+    remote_ip = socket.gethostbyname(host)
+    s.connect((remote_ip, port))
+    print('Socket Connected to ' + host + ' on ip ' + remote_ip + ' and port ' + str(port))
+
+    while True:
+        try:
             print('请选择要进行的操作：\n'
                   'upload\tupload a file to server file base\n'
                   'download\tdownload a file from server file base'
