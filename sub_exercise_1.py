91d08bb830f3e2424c0ba998b79d47254fd0e708
tyronedong <dtz1994@outlook.com>
2017-11-09 11:28:01 +0800

    add client

--- sub_exercise_1.py
+++ sub_exercise_1.py
@@ -14,21 +14,64 @@ import sys
 #
 # print('Ip address of ' + host + ' is ' + remote_ip)
 
+def client():
+    host = 'localhost'
+    port = 8888
+
+    try:
+        # create an AF_INET, STREAM socket (TCP)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('Socket Created')
+    except socket.error as msg:
+        print('Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1])
+        sys.exit()
+
+    remote_ip = socket.gethostbyname(host)
+    print(remote_ip)
+    print(port)
+    s.connect((remote_ip, port))
+    print('Socket Connected to ' + host + ' on ip ' + remote_ip)
+
+    while True:
+        try:
+            print('请投骰子(输入"go")')
+            cmd = sys.stdin.readline()
+            if cmd == 'exit':
+                break
+
+            # Set the whole string
+            s.sendall(bytes(cmd, encoding='utf-8'))
+            print('Message send successfully')
+
+            # Now receive data
+            reply = s.recv(4096)
+
+            print(str(reply, encoding='utf-8'))
+
+        except socket.error:
+            # Send failed
+            print('Send failed')
+            sys.exit()
+
+    s.close()
 
 def server():
     HOST = ''  # Symbolic name meaning all available interfaces
-    PORT = 5678  # Arbitrary non-privileged port
+    PORT = 8888  # Arbitrary non-privileged port
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Socket created')
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('Socket created')
+    except socket.error as msg:
+        print('Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1])
+        sys.exit()
 
     try:
         s.bind((HOST, PORT))
+        print('Socket bind complete')
     except socket.error as msg:
         print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
         sys.exit()
-
-    print('Socket bind complete')
 
     s.listen(10)
     print('Socket now listening on port: ' + str(PORT))
@@ -39,12 +82,14 @@ def server():
         conn, addr = s.accept()
         print('Connected with ' + addr[0] + ':' + str(addr[1]))
 
-        data = conn.recv(1024)
-        reply = 'OK...' + data
+        data = conn.recv(4096)
+        #cmd_rece
+        reply = 'OK...' + str(data, encoding='utf-8')
         if not data:
             break
 
-        conn.sendall(reply)
+        conn.sendall(bytes(reply, encoding='utf-8'))
+        print(reply)
 
     conn.close()
     s.close()
@@ -54,6 +99,8 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('method not specified(server or client)')
         exit()
-    server()
-    print('')
-    pass
+    if sys.argv[1] == 'server':
+        server()
+    elif sys.argv[1] == 'client':
+        client()
+    print('finish')
