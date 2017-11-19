91c8f71eee7cda157c2c17efdcc26db6cfe59207
tyronedong <dtz1994@outlook.com>
2017-11-19 23:53:58 +0800

    exercise1 improve

--- sub_exercise_1.py
+++ sub_exercise_1.py
@@ -17,17 +17,103 @@ import random as rd
 
 def stand_alone():
     while True:
-        print('请投骰子(输入"go")')
+        print('请输入算术表达式：')
         cmd = str(sys.stdin.readline()).strip('\n')
         if cmd == 'exit':
             break
-        elif cmd == 'go':
-            reply = str(rd.randint(1, 6))
         else:
-            reply = 'command not defined: ' + cmd
+            if not cmd.endswith('#'):
+                cmd += '#'
+            res = cal_expression(cmd)
 
-        print('骰子点数： ' + reply)
+        print('计算结果： ' + str(res))
         print('')
+
+OPTR_PRI = [[1, 1, -1, -1, -1, 1, 1],
+            [1, 1, -1, -1, -1, 1, 1],
+            [1, 1, 1, 1, -1, 1, 1],
+            [1, 1, 1, 1, -1, 1, 1],
+            [-1, -1, -1, -1, -1, 0, 2],
+            [-1, -1, -1, -1, 2, 1, 1],
+            [-1, -1, -1, -1, -1, 2, 0]]
+OPTR_DIC = {'+': 0, '-': 1, '*': 2, '/': 3, '(': 4, ')': 5, '#': 6}
+OPTR_LIST = OPTR_DIC.keys()
+NUM_LIST = ['0','1','2','3','4','5','6','7','8','9','.']
+
+def cal_expression(expression):
+    num_stack = []
+    optr_stack = []
+    optr_stack.append('#')
+
+    if not check_legalcy(expression):
+        print('illegal expression: ' + expression[:len(expression)-1])
+        return None
+
+    index = 0
+    while True:
+        if index >= len(expression):
+            break
+        chr = expression[index]
+
+        if chr in NUM_LIST:
+            val, l = read_float(expression, index)
+            num_stack.append(val)
+            index+=l
+        elif chr in OPTR_LIST:
+            top = optr_stack[len(optr_stack)-1]
+            pri = OPTR_PRI[OPTR_DIC[top]][OPTR_DIC[chr]]
+            if pri == 1:
+                try:
+                    top = optr_stack.pop()
+                    num_2 = num_stack.pop()
+                    num_1 = num_stack.pop()
+                    num_stack.append(operate(num_1, top, num_2))
+                except Exception as e:
+                    print('illegal expression: ' + expression[:len(expression)-1])
+                    break
+            elif pri == -1:
+                optr_stack.append(chr)
+                index+=1
+            elif pri == 0:
+                optr_stack.pop()
+                index+=1
+            else:
+                print('illegal expression: '+expression[:len(expression)-1])
+                break
+        else:
+            print('illegal expression: '+expression[:len(expression)-1])
+            break
+    if len(optr_stack) == 0 and len(num_stack) == 1:
+        return num_stack.pop()
+    else:
+        return None
+
+def check_legalcy(expression):
+    for s in expression:
+        if not (s in OPTR_DIC or s in NUM_LIST):
+            return False
+    return True
+
+def read_float(expression, start_index):
+    tmp = ''
+    for s in expression[start_index:]:
+        if s in NUM_LIST:
+           tmp+=s
+        else:
+            return float(tmp), len(tmp)
+    return None
+
+def operate(num_1, opt, num_2):
+    if opt == '+':
+        return num_1+num_2
+    elif opt == '-':
+        return num_1-num_2
+    elif opt == '*':
+        return num_1*num_2
+    elif opt == '/':
+        return num_1/num_2
+    else:
+        return None
 
 def client():
     host = 'localhost'
@@ -49,18 +135,14 @@ def client():
 
     while True:
         try:
-            print('请投骰子(输入"go")')
+            print('请输入算术表达式：')
             cmd = str(sys.stdin.readline()).strip('\n')
             if cmd == 'exit':
                 break
-
             # Set the whole string
             s.sendall(bytes(cmd, encoding='utf-8'))
-            # print('Message send successfully')
-
-            # Now receive data
             reply = s.recv(4096)
-            print('骰子点数： ' + str(reply, encoding='utf-8').strip())
+            print('计算结果： ' + str(reply, encoding='utf-8'))
             print('')
         except socket.error:
             # Send failed
@@ -103,10 +185,9 @@ def server():
             break
 
         cmd_recv = str(data, encoding='utf-8').strip()
-        if cmd_recv == 'go':
-            reply = str(rd.randint(1, 6))
-        else:
-            reply = 'command not defined: ' + cmd_recv
+        if not cmd_recv.endswith('#'):
+            cmd_recv += '#'
+        reply = str(cal_expression(cmd_recv))
 
         conn.sendall(bytes(reply, encoding='utf-8'))
         print('replt to client: ' + reply)
@@ -127,4 +208,5 @@ if __name__ == '__main__':
         stand_alone()
     # client()
     # server()
+    # stand_alone()
     print('finish')
